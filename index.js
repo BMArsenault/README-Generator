cbfa0e4db410f03d25d47044233429356c4ddb28
Brian <BMArsenault@gmail.com>
2021-11-29 12:05:39 -0700

    Added prompt questions

---
+++ index.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const generatePage = required('./readme-template.js');
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Please provide a link to your Github project. (Required)',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your Github link!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email address. (Required)',
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log('Please enter your email address!');
+          return false;
+        }
+      }
+    },
+  ])
+}
+
+const promptReadme = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is the name of your project? (Required)',
+            validate: projectInput => {
+              if (projectInput) {
+                return true;
+              } else {
+                console.log('Please provide project name!');
+                return false;
+              }
+            }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Please give a brief description of the project. (Required)',
+          validate: decsriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('Please enter project description!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What did you build this project with? (Check all that apply)',
+          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+    ])
+}
